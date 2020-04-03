14545cfbcdc0c8a626607f7230bbe3370a8f0caa
felipe.brum <felipe.brum@creditas.com.br>
2020-04-03 15:56:45 -0300

    Layout CSS

--- project/main.js
+++ project/main.js
@@ -1,5 +1,23 @@
+'use strict'
 var employee = {
-    name: '' ,
+    firstname: '',
+    lastname: '',
     email: '',
-    age: 0,
+    age: 0
 };
+
+document.querySelector('.btn-add').addEventListener('click', function() {
+    employee.name = document.querySelector('.name').value;
+    employee.email = document.querySelector('.email').value;
+    employee.age = document.querySelector('.age').value;
+    document.querySelector('.age').value = 2020 - employee.age;
+    implementMoreTables();
+});
+
+function implementMoreTables(){
+    var node = document.createElement("TR"); 
+    var t =  
+        document.createTextNode(); 
+    node.appendChild(t); 
+    document.getElementById("tbody").appendChild(node); 
+}

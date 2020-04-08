d88e2328e150da04c0156923d9afec9b0d7e6e3b
felipe.brum <felipe.brum@creditas.com.br>
2020-04-07 21:00:11 -0300

    Create Add Employeer Function

--- project/main.js
+++ project/main.js
@@ -1,23 +1,81 @@
 'use strict'
+
+var contUser = 0;
+
 var employee = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    age: 0
+    idUser: [],
+    completeName: [],
+    email: [],
+    age: [],
+    calcAge: function(year) 
+    {
+        this.age[contUser] = 2020 - year; 
+    }
 };
 
+initial();
+
+function initial()
+{
+    addElementTr();
+}
+
+function addElementTr()
+{
+
+    var tr = document.createElement('tr');
+    document.getElementById('tbody').appendChild(tr);
+    addElementTd(tr)
+}
+
+function addElementTd(tr)
+{ 
+    for(var i = 0; i < 4; i++) {
+        var td = document.createElement('td');  
+        if(i != 0) {
+            td.appendChild(addElementInput(i));
+        } else {
+            td.textContent = contUser;
+        }
+        tr.appendChild(td);
+    }
+}
+
+function addElementInput(i)
+{ 
+    var input = document.createElement('input');
+    switch(i) {
+        case 0: 
+        break; 
+        case 1:
+            input.placeholder = 'Name';
+            input.classList.add('Name-'+ contUser);
+        break;
+        case 2:
+            input.placeholder = 'Email';
+            input.classList.add('Email-'+ contUser);
+        break;
+        case 3:
+            input.placeholder = 'Birth Year';
+            input.classList.add('Year-'+ contUser);
+        break;
+    }
+    return input;
+}
+
+
 document.querySelector('.btn-add').addEventListener('click', function() {
-    employee.name = document.querySelector('.name').value;
-    employee.email = document.querySelector('.email').value;
-    employee.age = document.querySelector('.age').value;
-    document.querySelector('.age').value = 2020 - employee.age;
-    implementMoreTables();
+    employee.idUser[contUser] = contUser;
+    employee.completeName[contUser] = document.querySelector('.Name-'+ contUser).value;
+    employee.email[contUser] = document.querySelector('.Email-'+ contUser).value;
+    employee.calcAge(document.querySelector('.Year-'+ contUser).value);
+    display();
+    contUser++;
+    addElementTr();
 });
 
-function implementMoreTables(){
-    var node = document.createElement("TR"); 
-    var t =  
-        document.createTextNode(); 
-    node.appendChild(t); 
-    document.getElementById("tbody").appendChild(node); 
+function display() {
+    document.querySelector('.Name-'+ contUser).value = employee.completeName[contUser];
+    document.querySelector('.Email-'+ contUser).value = employee.email[contUser];
+    document.querySelector('.Year-'+ contUser).value = employee.age[contUser];
 }

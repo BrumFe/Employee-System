100454ba77a7ec0c9bb2d27a77a73b6a16b10210
felipe.brum <felipe.brum@creditas.com.br>
2020-04-10 02:01:08 -0300

    Project Repair

--- project/main.js
+++ project/main.js
@@ -1,21 +1,23 @@
 'use strict'
+/*
+<th>
+    <button class="btn btn-default btn-circle btn-add">
+        <i class="fa fa-plus"></i>
+    </button>
+
+    <button class="btn btn-default btn-circle btn-edit">
+        <i class="fa fa-refresh"></i>
+    </button>
+
+    <button class="btn btn-default btn-circle btn-delete">
+        <i class="fa fa-minus"></i>
+    </button>
+</th>
+*/
 
 var contUser = 0;
 
 var employees = [];
-
-/*var employee = {
-    idUser: [],
-    completeName: [],
-    email: [],
-    birthYear: [],
-    age: [],
-    calcAge: function(positionUser) 
-    {
-        this.age[positionUser] = 2020 - this.birthYear[positionUser]; 
-    }
-};*/
-
 
 initial();
 
@@ -36,12 +38,12 @@ function addElementTd(tr)
 { 
     for(var i = 0; i < 5; i++) {
         var td = document.createElement('td');  
-        if (i != 0 && i != 1) {
+        if(i >= 1 && i <= 3) {
             td.appendChild(addElementInput(i));
-        } else if (i == 1) {
-            td.textContent = contUser;
+        } else if (i === 0){
+            td.textContent = contUser;;
         } else {
-            td.textContent = 'Actions'
+            addElementButton(td);
         }
         tr.appendChild(td);
     }
@@ -51,15 +53,15 @@ function addElementInput(i)
 { 
     var input = document.createElement('input');
     switch(i) {
-        case 2:
+        case 1:
             input.placeholder = 'Name';
             input.classList.add('Name-'+ contUser);
         break;
-        case 3:
+        case 2:
             input.placeholder = 'Email';
             input.classList.add('Email-'+ contUser);
         break;
-        case 4:
+        case 3:
             input.placeholder = 'mm/dd/yyyy';
             input.classList.add('BirthDate-'+ contUser);
         break;
@@ -67,8 +69,43 @@ function addElementInput(i)
     return input;
 }
 
+function addElementButton(td) {
+    var button, addClassButton;
+    
+    for(var i = 0; i < 3; i++) {   
+        button = document.createElement('button');
+        addClassButton = button.classList;
+        addClassButton.add('btn');
+        addClassButton.add('btn-default');
+        addClassButton.add('btn-circle');
+        if (i === 0) {
+            addClassButton.add('btn-add-'+ contUser);
+        } else if (i === 1) {
+            addClassButton.add('btn-edit-'+ contUser);
+        } else {
+            addClassButton.add('btn-delete-'+ contUser);
+        }
+        button.appendChild(addElementIcon(i));
+        td.appendChild(button);
+    }
+}
 
-document.querySelector('.btn-add').addEventListener('click', function() {   
+function addElementIcon(cont) {
+    var icon = document.createElement('i');
+    var addClassIcon = icon.classList;
+    addClassIcon.add('fa');
+
+    if (cont === 0) {
+        addClassIcon.add('fa-plus');
+    } else if (cont === 1) {
+        addClassIcon.add('fa-refresh');
+    } else {
+        addClassIcon.add('fa-minus');
+    }
+    return icon; 
+}
+
+document.querySelector('.btn-add-'+ contUser).addEventListener('click', function() {   
     addEmployeer();
     display(contUser);
     contUser++;
